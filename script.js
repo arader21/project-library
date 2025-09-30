3a4bd5d3170ff9c2488913de96979cd391f9a2fc
Austin Rader <austinrader21@gmail.com>
2025-09-30 12:53:30 -0600

    form added to html, div library, and javascript added

---
+++ script.js
@@ -0,0 +1,33 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+function render() {
+    let libraryBook = document.getElementById("library");
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log(myLibrary[i]);
+    }}
+function addBookToLibrary() {
+    let title = document.querySelector("#title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    render();
+}
+let newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", function() {
+    let newBookForm = document.querySelector("#new-book-form");
+    newBookForm.style.display = "block";
+})
+
+document.querySelector("#new-book-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    addBookToLibrary();
+})
+
